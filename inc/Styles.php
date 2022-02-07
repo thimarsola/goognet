56bf0490d6f04aba406b2d993125dc97cf39a788
Marsola <tmarsola94@gmail.com>
2022-02-07 09:53:42 -0300

    gitflow-release-stash: 1.1.1

--- inc/Styles.php
+++ inc/Styles.php
@@ -4,27 +4,27 @@
 function loadCSS()
 {
     if(is_home()){
-        wp_enqueue_style('style', get_template_directory_uri() . '/assets/css/style.min.css', [], '1.0.5', 'all');
+        wp_enqueue_style('style', get_template_directory_uri() . '/assets/css/style.min.css', [], null, 'all');
     }elseif (is_page('Divulgação Google') || is_page('Divulgação Face & Insta') || is_page('Criação Website') || is_page('Identidade Visual') || is_page('Criação de Imagens') || is_page('Criação E-commerce')){
-        wp_enqueue_style('services', get_template_directory_uri() . '/assets/css/style-services.min.css', [], '1.0.5', 'all');
+        wp_enqueue_style('services', get_template_directory_uri() . '/assets/css/style-services.min.css', [], null, 'all');
     }elseif (is_page('Blog')){
-        wp_enqueue_style('blog', get_template_directory_uri() . '/assets/css/style-blog.min.css', [], '1.0.3', 'all');
+        wp_enqueue_style('blog', get_template_directory_uri() . '/assets/css/style-blog.min.css', [], null, 'all');
     }elseif (is_page('Projetos')){
-        wp_enqueue_style('projects', get_template_directory_uri() . '/assets/css/style-projects.min.css', [], '1.0.3', 'all');
+        wp_enqueue_style('projects', get_template_directory_uri() . '/assets/css/style-projects.min.css', [], null, 'all');
     }elseif (is_page('Simulador')){
-        wp_enqueue_style('simulator', get_template_directory_uri() . '/assets/css/style-simulator.min.css', [], '1.0.3', 'all');
+        wp_enqueue_style('simulator', get_template_directory_uri() . '/assets/css/style-simulator.min.css', [], null, 'all');
     }elseif (is_page('Simulador Google')){
         wp_enqueue_style('simulator-google', get_template_directory_uri() . '/assets/css/style-simulator-google.min.css', [], '1.0.0', 'all');
     }elseif (is_page('Consultoria')){
         wp_enqueue_style('consultancy', get_template_directory_uri() . '/assets/css/style-consultancy.min.css', [], '1.0.0', 'all');
     }elseif (is_page()){
-        wp_enqueue_style('page', get_template_directory_uri() . '/assets/css/style-product.min.css', [], '1.0.3', 'all');
+        wp_enqueue_style('page', get_template_directory_uri() . '/assets/css/style-product.min.css', [], null, 'all');
     }elseif (is_single()){
-        wp_enqueue_style('single', get_template_directory_uri() . '/assets/css/style-single.min.css', [], '1.0.3', 'all');
+        wp_enqueue_style('single', get_template_directory_uri() . '/assets/css/style-single.min.css', [], null, 'all');
     }elseif (is_archive()){
-	    wp_enqueue_style('category', get_template_directory_uri() . '/assets/css/style-category.min.css', [], '1.0.3', 'all');
+	    wp_enqueue_style('category', get_template_directory_uri() . '/assets/css/style-category.min.css', [], null, 'all');
     }elseif(is_404()){
-        wp_enqueue_style('error', get_template_directory_uri() . '/assets/css/style-error.min.css', [], '1.0.4', 'all');
+        wp_enqueue_style('error', get_template_directory_uri() . '/assets/css/style-error.min.css', [], null, 'all');
     }
 }
 
