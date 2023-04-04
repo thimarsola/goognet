21f8edae9d2fc79fc80b2b7ecd75c017f8340824
Marsola <tmarsola94@gmail.com>
2023-04-04 09:33:08 -0300

    fix: remove svg and include img

--- template-parts/home/home-google-partner.php
+++ template-parts/home/home-google-partner.php
@@ -3,14 +3,14 @@
     <header class="google__header">
         <div class="container">
             <!-- wrapper -->
-             <div class="google__header__wrapper">
-                 <div>
-                     <h2>Agência <span>G</span><span>o</span><span>o</span><span>g</span><span>l</span><span>e</span> <b>Partner</b></h2>
-                     <p>O que significa isso na prática?</p>
-                 </div>
+            <div class="google__header__wrapper">
+                <div>
+                    <h2>Agência <span>G</span><span>o</span><span>o</span><span>g</span><span>l</span><span>e</span> <b>Partner</b></h2>
+                    <p>O que significa isso na prática?</p>
+                </div>
 
-                 <a rel="nofollow noopener noreferrer" href="https://www.google.com/partners/agency?id=2994047330" class="btn btn-g-blue-500" target="_blank" title="Conheça o nosso selo Google Partner">Confira o nosso Selo</a>
-             </div>
+                <a rel="nofollow noopener noreferrer" href="https://www.google.com/partners/agency?id=2994047330" class="btn btn-g-blue-500" target="_blank" title="Conheça o nosso selo Google Partner">Confira o nosso Selo</a>
+            </div>
             <!-- end of wrapper -->
         </div>
     </header>
@@ -22,28 +22,28 @@
             <!-- card -->
             <article class="google__row__card">
                 <!-- icon -->
-                 <div class="google__row__card__icon">
-		 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="49.796" viewBox="0 0 50 49.796">
-  <g data-name="Grupo 2650" transform="translate(6411.404 2746)">
-    <path data-name="Caminho 2127" d="M27.66,49.8a7.112,7.112,0,0,1-6.18-3.589l1.29-17.792,4.891-4.15V42.683H63.229V32.012a3.547,3.547,0,0,1,1.1-2.577c2.245,2.708,4.336,5.237,6.009,7.269v5.979A7.114,7.114,0,0,1,63.229,49.8ZM43.67,34.551,33.61,24.491q-2.515-2.515,0-5.03t5.03,0L46.4,27.222l8.793-8.793c1.457,1.752,3.007,3.617,4.566,5.494L49.168,34.516a3.593,3.593,0,0,1-2.56,1.257,2.58,2.58,0,0,1-.423.035A3.624,3.624,0,0,1,43.67,34.551Z" transform="translate(-6431.95 -2746)" fill="#286bef"/>
-    <path data-name="Caminho 2128" d="M58.242,65.6a7.114,7.114,0,0,1-7.114-7.114V23.44l7.114,3.482V58.486H71.086L73.258,65.6Zm5.95-25.305q-2.438-2.438-.15-4.875l2.077,6.8Z" transform="translate(-6462.532 -2761.804)" fill="#179c52"/>
-    <path id="Caminho_2130" data-name="Caminho 2130" d="M31.265,46.9a3.613,3.613,0,0,1,1.257-2.529q2.515-2.516,5.03,0l2.107,2.107-4.412,2.011Zm-11.806-4.71V32.024a7.114,7.114,0,0,1,7.114-7.114H47.914a3.557,3.557,0,1,1,0,7.114H26.573v13Zm43.712-7.913A3.258,3.258,0,0,1,66.335,33.1l.415,1.028L60.442,37Z" transform="translate(-6430.863 -2770.91)" fill="#f7b529"/>
-    <path data-name="Caminho 2129" d="M42.683,71.614V70.79a3.557,3.557,0,1,1,7.114,0V78.1ZM30.946,60.909,43.712,48.143q2.515-2.515,5.03,0t0,5.03L36.207,65.708ZM30.54,39.452a3.558,3.558,0,0,1-.871,6.226Z" transform="translate(-6411.404 -2784.777)" fill="#fe3e30"/>
-  </g>
-                 </svg>
+                <div class="google__row__card__icon">
+                    <img src="<?= image('google.svg') ?>" alt="A Goognet Solução Digital é um Google Partner" loading="lazy">
+                    <g data-name="Grupo 2650" transform="translate(6411.404 2746)">
+                        <path data-name="Caminho 2127" d="M27.66,49.8a7.112,7.112,0,0,1-6.18-3.589l1.29-17.792,4.891-4.15V42.683H63.229V32.012a3.547,3.547,0,0,1,1.1-2.577c2.245,2.708,4.336,5.237,6.009,7.269v5.979A7.114,7.114,0,0,1,63.229,49.8ZM43.67,34.551,33.61,24.491q-2.515-2.515,0-5.03t5.03,0L46.4,27.222l8.793-8.793c1.457,1.752,3.007,3.617,4.566,5.494L49.168,34.516a3.593,3.593,0,0,1-2.56,1.257,2.58,2.58,0,0,1-.423.035A3.624,3.624,0,0,1,43.67,34.551Z" transform="translate(-6431.95 -2746)" fill="#286bef" />
+                        <path data-name="Caminho 2128" d="M58.242,65.6a7.114,7.114,0,0,1-7.114-7.114V23.44l7.114,3.482V58.486H71.086L73.258,65.6Zm5.95-25.305q-2.438-2.438-.15-4.875l2.077,6.8Z" transform="translate(-6462.532 -2761.804)" fill="#179c52" />
+                        <path id="Caminho_2130" data-name="Caminho 2130" d="M31.265,46.9a3.613,3.613,0,0,1,1.257-2.529q2.515-2.516,5.03,0l2.107,2.107-4.412,2.011Zm-11.806-4.71V32.024a7.114,7.114,0,0,1,7.114-7.114H47.914a3.557,3.557,0,1,1,0,7.114H26.573v13Zm43.712-7.913A3.258,3.258,0,0,1,66.335,33.1l.415,1.028L60.442,37Z" transform="translate(-6430.863 -2770.91)" fill="#f7b529" />
+                        <path data-name="Caminho 2129" d="M42.683,71.614V70.79a3.557,3.557,0,1,1,7.114,0V78.1ZM30.946,60.909,43.712,48.143q2.515-2.515,5.03,0t0,5.03L36.207,65.708ZM30.54,39.452a3.558,3.558,0,0,1-.871,6.226Z" transform="translate(-6411.404 -2784.777)" fill="#fe3e30" />
+                    </g>
+                    </svg>
                 </div>
                 <!-- end of icon -->
 
                 <!-- header -->
-                 <header class="google__row__card__header">
-                     <h3>Certificações</h3>
-                 </header>
+                <header class="google__row__card__header">
+                    <h3>Certificações</h3>
+                </header>
                 <!-- end of header -->
 
                 <!-- body -->
-                 <div class="google__row__card__body">
-                     <p>Para ser uma <strong><a rel="nofollow noopener noreferrer" href="https://www.google.com/partners/agency?id=2994047330" target="_blank" title="Conheça o nosso selo Google Partner">Agência Google Partner</a></strong> uma das premissas básicas é ter alto conhecimento na gestão de campanhas, uso da plataforma Google e uma grande quantidade de profissionais certificados nos principais formatos de anúncios do Google.</p>
-                 </div>
+                <div class="google__row__card__body">
+                    <p>Para ser uma <strong><a rel="nofollow noopener noreferrer" href="https://www.google.com/partners/agency?id=2994047330" target="_blank" title="Conheça o nosso selo Google Partner">Agência Google Partner</a></strong> uma das premissas básicas é ter alto conhecimento na gestão de campanhas, uso da plataforma Google e uma grande quantidade de profissionais certificados nos principais formatos de anúncios do Google.</p>
+                </div>
                 <!-- end of body -->
             </article>
             <!-- end of card -->
@@ -52,27 +52,27 @@
             <article class="google__row__card">
                 <!-- icon -->
                 <div class="google__row__card__icon">
-        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="49.796" viewBox="0 0 50 49.796">
-  <g data-name="Grupo 2650" transform="translate(6411.404 2746)">
-    <path data-name="Caminho 2127" d="M27.66,49.8a7.112,7.112,0,0,1-6.18-3.589l1.29-17.792,4.891-4.15V42.683H63.229V32.012a3.547,3.547,0,0,1,1.1-2.577c2.245,2.708,4.336,5.237,6.009,7.269v5.979A7.114,7.114,0,0,1,63.229,49.8ZM43.67,34.551,33.61,24.491q-2.515-2.515,0-5.03t5.03,0L46.4,27.222l8.793-8.793c1.457,1.752,3.007,3.617,4.566,5.494L49.168,34.516a3.593,3.593,0,0,1-2.56,1.257,2.58,2.58,0,0,1-.423.035A3.624,3.624,0,0,1,43.67,34.551Z" transform="translate(-6431.95 -2746)" fill="#286bef"/>
-    <path data-name="Caminho 2128" d="M58.242,65.6a7.114,7.114,0,0,1-7.114-7.114V23.44l7.114,3.482V58.486H71.086L73.258,65.6Zm5.95-25.305q-2.438-2.438-.15-4.875l2.077,6.8Z" transform="translate(-6462.532 -2761.804)" fill="#179c52"/>
-    <path data-name="Caminho 2130" d="M31.265,46.9a3.613,3.613,0,0,1,1.257-2.529q2.515-2.516,5.03,0l2.107,2.107-4.412,2.011Zm-11.806-4.71V32.024a7.114,7.114,0,0,1,7.114-7.114H47.914a3.557,3.557,0,1,1,0,7.114H26.573v13Zm43.712-7.913A3.258,3.258,0,0,1,66.335,33.1l.415,1.028L60.442,37Z" transform="translate(-6430.863 -2770.91)" fill="#f7b529"/>
-    <path data-name="Caminho 2129" d="M42.683,71.614V70.79a3.557,3.557,0,1,1,7.114,0V78.1ZM30.946,60.909,43.712,48.143q2.515-2.515,5.03,0t0,5.03L36.207,65.708ZM30.54,39.452a3.558,3.558,0,0,1-.871,6.226Z" transform="translate(-6411.404 -2784.777)" fill="#fe3e30"/>
-  </g>
-</svg>
+                    <img src="<?= image('google.svg') ?>" alt="A Goognet Solução Digital é um Google Partner" loading="lazy">
+                    <g data-name="Grupo 2650" transform="translate(6411.404 2746)">
+                        <path data-name="Caminho 2127" d="M27.66,49.8a7.112,7.112,0,0,1-6.18-3.589l1.29-17.792,4.891-4.15V42.683H63.229V32.012a3.547,3.547,0,0,1,1.1-2.577c2.245,2.708,4.336,5.237,6.009,7.269v5.979A7.114,7.114,0,0,1,63.229,49.8ZM43.67,34.551,33.61,24.491q-2.515-2.515,0-5.03t5.03,0L46.4,27.222l8.793-8.793c1.457,1.752,3.007,3.617,4.566,5.494L49.168,34.516a3.593,3.593,0,0,1-2.56,1.257,2.58,2.58,0,0,1-.423.035A3.624,3.624,0,0,1,43.67,34.551Z" transform="translate(-6431.95 -2746)" fill="#286bef" />
+                        <path data-name="Caminho 2128" d="M58.242,65.6a7.114,7.114,0,0,1-7.114-7.114V23.44l7.114,3.482V58.486H71.086L73.258,65.6Zm5.95-25.305q-2.438-2.438-.15-4.875l2.077,6.8Z" transform="translate(-6462.532 -2761.804)" fill="#179c52" />
+                        <path data-name="Caminho 2130" d="M31.265,46.9a3.613,3.613,0,0,1,1.257-2.529q2.515-2.516,5.03,0l2.107,2.107-4.412,2.011Zm-11.806-4.71V32.024a7.114,7.114,0,0,1,7.114-7.114H47.914a3.557,3.557,0,1,1,0,7.114H26.573v13Zm43.712-7.913A3.258,3.258,0,0,1,66.335,33.1l.415,1.028L60.442,37Z" transform="translate(-6430.863 -2770.91)" fill="#f7b529" />
+                        <path data-name="Caminho 2129" d="M42.683,71.614V70.79a3.557,3.557,0,1,1,7.114,0V78.1ZM30.946,60.909,43.712,48.143q2.515-2.515,5.03,0t0,5.03L36.207,65.708ZM30.54,39.452a3.558,3.558,0,0,1-.871,6.226Z" transform="translate(-6411.404 -2784.777)" fill="#fe3e30" />
+                    </g>
+                    </svg>
                 </div>
                 <!-- end of icon -->
 
                 <!-- header -->
-                 <header class="google__row__card__header">
-                     <h3>Investimentos</h3>
-                 </header>
+                <header class="google__row__card__header">
+                    <h3>Investimentos</h3>
+                </header>
                 <!-- end of header -->
 
                 <!-- body -->
-                 <div class="google__row__card__body">
-                     <p>Outro fator determinante para se tornar uma <strong><a rel="nofollow noopener noreferrer" href="https://www.google.com/partners/agency?id=2994047330" target="_blank" title="Conheça o nosso selo Google Partner">Agência Google Partner</a></strong> é o alto volume de investimento da carteira de clientes em campanhas do Google Adwords. Assim como investimentos nos principais formatos: Search, Display, Shopping, Youtube e App.</p>
-                 </div>
+                <div class="google__row__card__body">
+                    <p>Outro fator determinante para se tornar uma <strong><a rel="nofollow noopener noreferrer" href="https://www.google.com/partners/agency?id=2994047330" target="_blank" title="Conheça o nosso selo Google Partner">Agência Google Partner</a></strong> é o alto volume de investimento da carteira de clientes em campanhas do Google Adwords. Assim como investimentos nos principais formatos: Search, Display, Shopping, Youtube e App.</p>
+                </div>
                 <!-- end of body -->
             </article>
             <!-- end of card -->
@@ -80,29 +80,21 @@
             <!-- card -->
             <article class="google__row__card">
                 <!-- icon -->
-		<div class="google__row__card__icon">
-		<svg xmlns="http://www.w3.org/2000/svg" width="50" height="49.796" viewBox="0 0 50 49.796">
-  <g data-name="Grupo 2650" transform="translate(6411.404 2746)">
-    <path data-name="Caminho 2127" d="M27.66,49.8a7.112,7.112,0,0,1-6.18-3.589l1.29-17.792,4.891-4.15V42.683H63.229V32.012a3.547,3.547,0,0,1,1.1-2.577c2.245,2.708,4.336,5.237,6.009,7.269v5.979A7.114,7.114,0,0,1,63.229,49.8ZM43.67,34.551,33.61,24.491q-2.515-2.515,0-5.03t5.03,0L46.4,27.222l8.793-8.793c1.457,1.752,3.007,3.617,4.566,5.494L49.168,34.516a3.593,3.593,0,0,1-2.56,1.257,2.58,2.58,0,0,1-.423.035A3.624,3.624,0,0,1,43.67,34.551Z" transform="translate(-6431.95 -2746)" fill="#286bef"/>
-    <path data-name="Caminho 2128" d="M58.242,65.6a7.114,7.114,0,0,1-7.114-7.114V23.44l7.114,3.482V58.486H71.086L73.258,65.6Zm5.95-25.305q-2.438-2.438-.15-4.875l2.077,6.8Z" transform="translate(-6462.532 -2761.804)" fill="#179c52"/>
-    <path data-name="Caminho 2130" d="M31.265,46.9a3.613,3.613,0,0,1,1.257-2.529q2.515-2.516,5.03,0l2.107,2.107-4.412,2.011Zm-11.806-4.71V32.024a7.114,7.114,0,0,1,7.114-7.114H47.914a3.557,3.557,0,1,1,0,7.114H26.573v13Zm43.712-7.913A3.258,3.258,0,0,1,66.335,33.1l.415,1.028L60.442,37Z" transform="translate(-6430.863 -2770.91)" fill="#f7b529"/>
-    <path data-name="Caminho 2129" d="M42.683,71.614V70.79a3.557,3.557,0,1,1,7.114,0V78.1ZM30.946,60.909,43.712,48.143q2.515-2.515,5.03,0t0,5.03L36.207,65.708ZM30.54,39.452a3.558,3.558,0,0,1-.871,6.226Z" transform="translate(-6411.404 -2784.777)" fill="#fe3e30"/>
-  </g>
-</svg>
-     
+                <div class="google__row__card__icon">
+                    <img src="<?= image('google.svg') ?>" alt="A Goognet Solução Digital é um Google Partner" loading="lazy">
                 </div>
                 <!-- end of icon -->
 
                 <!-- header -->
-                 <header class="google__row__card__header">
-                     <h3>Resultados</h3>
-                 </header>
+                <header class="google__row__card__header">
+                    <h3>Resultados</h3>
+                </header>
                 <!-- end of header -->
 
                 <!-- body -->
-                 <div class="google__row__card__body">
-                     <p>A análise dos parceiros qualificados como <strong><a rel="nofollow noopener noreferrer" href="https://www.google.com/partners/agency?id=2994047330" target="_blank" title="Conheça o nosso selo Google Partner">Agência Google Partner</a></strong> é feito constantemente. Para se manter como parceiro qualificado, a agência precisa manter um alto índice de resultados para seus clientes e uma boa gestão das campanhas.</p>
-                 </div>
+                <div class="google__row__card__body">
+                    <p>A análise dos parceiros qualificados como <strong><a rel="nofollow noopener noreferrer" href="https://www.google.com/partners/agency?id=2994047330" target="_blank" title="Conheça o nosso selo Google Partner">Agência Google Partner</a></strong> é feito constantemente. Para se manter como parceiro qualificado, a agência precisa manter um alto índice de resultados para seus clientes e uma boa gestão das campanhas.</p>
+                </div>
                 <!-- end of body -->
             </article>
             <!-- end of card -->
